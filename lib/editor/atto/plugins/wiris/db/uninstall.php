9ebc016efbb693512437adb7e56282b6a623f7a4
Ort Moodle developer <MoodleDev@ort.org.il>
2016-08-29 15:27:53 +0300

    lib/editor/atto/plugins/wiris (new)

---
+++ lib/editor/atto/plugins/wiris/db/uninstall.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+function xmldb_atto_wiris_uninstall() {
+    // Remove 'wiris' from the toolbar editor_atto config variable.
+    $toolbar = get_config('editor_atto', 'toolbar');
+    if (strpos($toolbar, 'wiris') !== false) {
+        $groups = explode("\n", $toolbar);
+        $newgroups = array();
+        foreach ($groups as $group) {
+            if (strpos($group, 'wiris') !== false) {
+                $parts = explode('=', $group);
+                $items = explode(',', $parts[1]);
+                $newitems = array();
+                foreach ($items as $item) {
+                    if (trim($item) != 'wiris') {
+                        $newitems[] = $item;
+                    }
+                }
+                if (!empty($newitems)) {
+                    $parts[1] = implode(',', $newitems);
+                    $newgroups[] = implode('=', $parts);
+                }
+            } else {
+                $newgroups[] = $group;
+            }
+        }
+        $toolbar = implode("\n", $newgroups);
+        set_config('toolbar', $toolbar, 'editor_atto');
+    }
+}
---
+++ lib/editor/atto/plugins/wiris/lib.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Initialise the js strings required for this module.
+ */
+function atto_wiris_strings_for_js() {
+    global $PAGE;
+    $PAGE->requires->strings_for_js(
+      array(
+        'wiris_editor_title',
+        'wiris_cas_title',
+        'wiris_chem_editor_title',
+      ),
+      'atto_wiris');
+}
+
+/**
+ * Set parameters to be passed to the js plugin constructor.
+ */
+function atto_wiris_params_for_js() {
+    return array('lang' => current_language());
+}
