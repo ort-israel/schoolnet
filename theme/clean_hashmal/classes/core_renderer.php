4c10b89e8d87e8c2657af4bcfbd1d4629bdb1fca
Ort Moodle developer <MoodleDev@ort.org.il>
2016-09-05 12:47:19 +0300

    theme/clean_hashmal/ (new)

---
+++ theme/clean_hashmal/classes/core_renderer.php
@@ -0,0 +1,198 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+require_once($CFG->dirroot . '/theme/bootstrapbase/renderers.php');
+
+/**
+ * Clean core renderers.
+ *
+ * @package    theme_clean
+ * @copyright  2015 Frédéric Massart - FMCorz.net
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class theme_clean_hashmal_core_renderer extends theme_bootstrapbase_core_renderer {
+
+    /**
+     * Either returns the parent version of the header bar, or a version with the logo replacing the header.
+     *
+     * @since Moodle 2.9
+     * @param array $headerinfo An array of header information, dependant on what type of header is being displayed. The following
+     *                          array example is user specific.
+     *                          heading => Override the page heading.
+     *                          user => User object.
+     *                          usercontext => user context.
+     * @param int $headinglevel What level the 'h' tag will be.
+     * @return string HTML for the header bar.
+     */
+    /*public function context_header($headerinfo = null, $headinglevel = 1) {
+        if ($headinglevel == 1 && !empty($this->page->theme->settings->logo)) {
+            return html_writer::tag('div', '', array('class' => 'logo'));
+        }
+        return parent::context_header($headerinfo, $headinglevel);
+    }*/
+
+    /**
+     * Either returns the parent version of the header bar, or a version with the logo replacing the header.
+     * @param context_header $contextheader
+     * @return string
+     */
+    protected function render_context_header(context_header $contextheader) {
+
+        // All the html stuff goes here.
+        $html = html_writer::start_div('page-context-header');
+
+        // Image data.
+        if (isset($contextheader->imagedata)) {
+            // Header specific image.
+            $html .= html_writer::div($contextheader->imagedata, 'page-header-image');
+        }
+
+        $logo = '';
+        // Big Logo, from settings.(Lea)
+        if (!empty($this->page->theme->settings->logo)) {
+            $alt = (!isset($contextheader->heading)) ? $this->page->heading : $contextheader->heading;
+            $logo = html_writer::empty_tag('img', array('src' => $this->page->theme->setting_file_url('logo', 'logo'), 'alt' => $alt, 'class' => 'logo_img'));
+        }
+
+        // Small Logo, from settings.(Lea)
+        if (!empty($this->page->theme->settings->logosmall)) {
+            $alt = (!isset($contextheader->heading)) ? $this->page->heading : $contextheader->heading;
+            $logo .= html_writer::empty_tag('img', array('src' => $this->page->theme->setting_file_url('logosmall', 'logosmall'), 'alt' => $alt, 'class' => 'logo_img_mob'));
+        }
+
+        // ort logo, from theme
+        $ort_logo = html_writer::link('http://ort.org.il', get_string('ortsite', 'theme_clean_hashmal'), array('class' => 'logo-ort'));
+
+        // Headings.
+        if (!isset($contextheader->heading)) {
+            $headings = $this->heading($logo, $contextheader->headinglevel);
+        } else {
+            $headings = $this->heading($logo, $contextheader->headinglevel);
+        }
+
+        $html .= html_writer::tag('div', $ort_logo . $headings, array('class' => 'page-header-headings'));
+
+        // Top menu - from settings
+        $html .= $this->get_settings_navbar();
+
+        // Buttons.
+        if (isset($contextheader->additionalbuttons)) {
+            $html .= html_writer::start_div('btn-group header-button-group');
+            foreach ($contextheader->additionalbuttons as $button) {
+                if (!isset($button->page)) {
+                    // Include js for messaging.
+                    if ($button['buttontype'] === 'message') {
+                        message_messenger_requirejs();
+                    }
+                    $image = $this->pix_icon($button['formattedimage'], $button['title'], 'moodle', array(
+                        'class' => 'iconsmall',
+                        'role' => 'presentation'
+                    ));
+                    $image .= html_writer::span($button['title'], 'header-button-title');
+                } else {
+                    $image = html_writer::empty_tag('img', array(
+                        'src' => $button['formattedimage'],
+                        'role' => 'presentation'
+                    ));
+                }
+                $html .= html_writer::link($button['url'], html_writer::tag('span', $image), $button['linkattributes']);
+            }
+            $html .= html_writer::end_div();
+        }
+
+        $html .= html_writer::end_div();
+
+        return $html;
+    }
+
+    private function get_settings_navbar() {
+        global $PAGE;
+
+
+        $num_of_items = 5;
+
+        $settings_items = array();
+
+        // convert to array for easier access
+        $settings_arr = (array)$PAGE->theme->settings;
+
+        for ($i = 0; $i < $num_of_items; $i++) {
+            $item = new stdClass();
+            $item->id = 'item' . ($i + 1);
+            $item->label_setting_name = $item->id . 'label';
+            $item->url_setting_name = $item->id . 'url';
+
+            // get the label and url
+            $item->label_setting_value =
+                !empty($settings_arr[$item->label_setting_name]) && !empty($settings_arr[$item->url_setting_name]) ? $settings_arr[$item->label_setting_name] : '';
+            $item->url_setting_value =
+                !empty($settings_arr[$item->label_setting_name]) && !empty($settings_arr[$item->url_setting_name]) ? $settings_arr[$item->url_setting_name] : '';
+
+            //create an html element if there are values
+            if (!empty($item->label_setting_value) && !empty($item->url_setting_value)) {
+                $item->link = html_writer::link($item->url_setting_value, $item->label_setting_value);
+                array_push($settings_items, $item->link);
+            }
+
+
+        }
+        $str = html_writer::alist($settings_items, array('class' => 'links-navigation'));
+        return $str;
+    }
+}
+
+
+if (class_exists('format_flexsections')) {
+    class theme_clean_hashmal_format_flexsections extends format_flexsections {
+        /**
+         * Course-specific information to be output immediately above content on any course page
+         *
+         * See {@link format_base::course_header()} for usage
+         *
+         * @return null|renderable null for no output or object with data for plugin renderer
+         */
+        public function course_content_header() {
+            global $PAGE;
+
+            // if we are on course view page for particular section, return 'back to parent' control
+            /* if ($this->get_viewed_section()) {
+                 $section = $this->get_section($this->get_viewed_section());
+                 if ($section->parent) {
+                     $sr = $this->find_collapsed_parent($section->parent);
+                     $text = new lang_string('backtosection', 'format_flexsections', $this->get_section_name($section->parent));
+                 } else {
+                     $sr = 0;
+                     $text = new lang_string('backtocourse', 'format_flexsections', $this->get_course()->fullname);
+                 }
+                 $url = $this->get_view_url($section->section, array('sr' => $sr));
+                 return new format_flexsections_edit_control('backto', $url, strip_tags($text));
+             }
+
+             // if we are on module view page, return 'back to section' control
+             if ($PAGE->context && $PAGE->context->contextlevel == CONTEXT_MODULE && $PAGE->cm) {
+                 $sectionnum = $PAGE->cm->sectionnum;
+                 if ($sectionnum) {
+                     $text = new lang_string('backtosection', 'format_flexsections', $this->get_section_name($sectionnum));
+                 } else {
+                     $text = new lang_string('backtocourse', 'format_flexsections', $this->get_course()->fullname);
+                 }
+                 return new format_flexsections_edit_control('backto', $this->get_view_url($sectionnum), strip_tags($text));
+             }*/
+
+            //return parent::course_content_header();
+        }
+    }
+}
---
+++ theme/clean_hashmal/lang/en/theme_clean_hashmal.php
@@ -0,0 +1,94 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_clean_hashmal', language 'en'
+ *
+ * @package   theme_clean_hashmal
+ * @copyright 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+<div class="well">
+<h2>Clean Hashmal</h2>
+<p><img class=img-polaroid src="clean_hashmal/pix/screenshot.jpg" /></p>
+</div>
+<div class="well">
+<h3>About</h3>
+<p>Clean Hashmal is a modified Moodle bootstrap theme which inherits styles and renderers from its parent theme.</p>
+<h3>Parents</h3>
+<p>This theme is based upon the Bootstrap theme, which was created for Moodle 2.5, with the help of:<br>
+Stuart Lamour, Mark Aberdour, Paul Hibbitts, Mary Evans.</p>
+<h3>Theme Credits</h3>
+<p>Authors: Bas Brands, David Scotson, Mary Evans<br>
+Contact: bas@sonsbeekmedia.nl<br>
+Website: <a href="http://www.basbrands.nl">www.basbrands.nl</a>
+</p>
+<h3>Report a bug:</h3>
+<p><a href="http://tracker.moodle.org">http://tracker.moodle.org</a></p>
+<h3>More information</h3>
+<p><a href="clean_hashmal/README.txt">How to copy and customise this theme.</a></p>
+</div></div>';
+
+$string['configtitle'] = 'Clean Hashmal';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+
+$string['invert'] = 'Invert navbar';
+$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'The logo is displayed in the header in high resolutions. ';
+$string['logo_small'] = 'Small Logo';
+$string['logosmalldesc'] = 'The logo is displayed in the header in low resolutions. ';
+
+$string['pluginname'] = 'Clean Hashmal';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['ortsite'] = 'Ort Site';
+
+$string['topmenu'] = 'Top Menu';
+$string['topmenudesc'] = 'Menu above breadcrumbs';
+$string['item1url'] = 'Item 1 url';
+$string['item1urldesc'] = 'Item URL 1 description';
+$string['item1label'] = 'Item 1 name';
+$string['item1labeldesc'] = 'Item name 1 description';
+$string['item2url'] = 'Item 2 url';
+$string['item2urldesc'] = 'Item URL 2 description';
+$string['item2label'] = 'Item 2 name';
+$string['item2labeldesc'] = 'Item name 2 description';
+$string['item3url'] = 'Item 3 url';
+$string['item3urldesc'] = 'Item URL 3 description';
+$string['item3label'] = 'Item 3 name';
+$string['item3labeldesc'] = 'Item name 3 description';
+$string['item4url'] = 'Item 4 url';
+$string['item4urldesc'] = 'Item URL 4 description';
+$string['item4label'] = 'Item 4 name';
+$string['item4labeldesc'] = 'Item name 4 description';
+$string['item5url'] = 'Item 5 url';
+$string['item5urldesc'] = 'Item URL 5 description';
+$string['item5label'] = 'Item 5 name';
+$string['item5labeldesc'] = 'Item name 5 description';
+
+
+
