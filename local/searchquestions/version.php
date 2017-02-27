0e49be072015e98559497a9eb37f10c332e84446
root <root@dev.moodle.ort.org.il>
2017-02-27 10:55:33 +0200

    local/searchquestions/ (new) search in question quiz bank

---
+++ local/searchquestions/version.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for component 'local_searchquestions'
+ *
+ * @package   local_searchquestions
+ * @copyright 2014 onwards Ray Morris
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$plugin->version   = 2015011400;
+$plugin->release   = '2.2.7';
+$plugin->maturity  = MATURITY_RC;
+$plugin->requires  = 2014041100;
+$plugin->component = 'local_searchquestions';
+
