46588e61efdc16b034338e1b3f64b9a45df3c40c
root <root@mapa-appsmoodle1.ortweb.org>
2018-07-08 11:32:27 +0300

    MDL-62275 qtype_calc*: improve validation of formulae

--- question/type/calculatedmulti/questiontype.php
+++ question/type/calculatedmulti/questiontype.php
@@ -231,40 +231,29 @@ class qtype_calculatedmulti extends qtype_calculated {
 
     public function comment_on_datasetitems($qtypeobj, $questionid, $questiontext,
             $answers, $data, $number) {
-        global $DB;
+        
         $comment = new stdClass();
         $comment->stranswers = array();
         $comment->outsidelimit = false;
         $comment->answers = array();
 
         $answers = fullclone($answers);
-        $errors = '';
-        $delimiter = ': ';
+        
+		
         foreach ($answers as $key => $answer) {
+			// Evaluate the equations i.e {=5+4).
             $anssubstituted = $this->substitute_variables($answer->answer, $data);
-            // Evaluate the equations i.e {=5+4).
-            $anstext = '';
-            $anstextremaining = $anssubstituted;
-            while (preg_match('~\{=([^[:space:]}]*)}~', $anstextremaining, $regs1)) {
-                $anstextsplits = explode($regs1[0], $anstextremaining, 2);
-                $anstext =$anstext.$anstextsplits[0];
-                $anstextremaining = $anstextsplits[1];
-                if (empty($regs1[1])) {
-                    $str = '';
+            $formulas = $this->find_formulas($anssubstituted);
+            $replaces = [];
+            foreach ($formulas as $formula) {
+                if ($formulaerrors = qtype_calculated_find_formula_errors($formula)) {
+                    $str = $formulaerrors;
                 } else {
-                    if ($formulaerrors = qtype_calculated_find_formula_errors($regs1[1])) {
-                        $str=$formulaerrors;
-                    } else {
-                        eval('$str = '.$regs1[1].';');
-                    }
+                    eval('$str = ' . $formula . ';');
                 }
-                $anstext = $anstext.$str;
+                $replaces[$formula] = $str;
             }
-<<<<<<< HEAD
-            $anstext .= $anstextremaining;
-=======
             $anstext = str_replace(array_keys($replaces), array_values($replaces), $anssubstituted);
->>>>>>> 6ed62ae... MDL-62275 qtype_calculatedmulti: Fix typo
             $comment->stranswers[$key] = $anssubstituted.'<br/>'.$anstext;
         }
         return fullclone($comment);
